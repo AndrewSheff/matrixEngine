3c4791507d8082bde31674ce7fd220c148f4af4e
andrewshevtsov <andrei.shevtsov@aisa.ru>
2025-09-22 19:37:46 +0300

    6

---
+++ include/item.h
@@ -0,0 +1,53 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+typedef enum
+{
+    SUPPLY,
+    WEAPON,
+    TOOL
+} item_type;
+
+typedef enum
+{
+    //TODO надо придумать
+} effect_type;
+
+//для каждого типа свои параметры урона для каждого типа урона
+typedef enum
+{
+    SWORD,
+    KNIFE,
+    DAGGER,
+    HAMMER
+    //TODO дополнить
+} weapon_type;
+
+typedef enum
+{
+    PRICKING,
+    CUTING,
+    HACKING,
+    HITING
+} damage_type;
+
+typedef struct
+{
+    item_type type,
+    char* name
+} item_base_info;
+
+typedef struct
+{
+    item_base_info item,
+    effect_type effect,
+    int satiety
+} item_food;
+
+typedef struct
+{
+    item_base_info item,
+    weapon_type type,
+} item_weapon;
+
+#endif
